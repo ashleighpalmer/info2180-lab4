62406ba9b671d3487368cad792b422a26c948263
ashleighpalmer <kriscela904@gmail.com>
2022-11-07 22:54:08 -0500

    Edited files

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 window.onload = function(){
     var search = document.getElementById("btn-search");
-    var inputfield = document.getElementById("enter");
+    var inputfield = document.getElementById("search-field-input");
     var result = document.getElementById("result");
     search.addEventListener("click", (e)=>{
         e.preventDefault();
         console.log("click");
 
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'http:localhost/info2180-lab4/superheroes.php', true);
+        httpRequest.open('GET', 'http:localhost/info2180-lab4/superheroes.php?query='+inputfield.value, true);
         httpRequest.send();
    
         httpRequest.onreadystatechange = function() {
             if(httpRequest.readyState === XMLHttpRequest.DONE){
                 if(httpRequest.status == 200) {
                     var results = httpRequest.responseText;
-                   alert(results)
+                   result.innerHTML=results
             } else {
                 alert("Error");
                 }
